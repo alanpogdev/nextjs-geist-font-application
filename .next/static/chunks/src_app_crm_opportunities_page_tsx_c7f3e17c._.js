685045c20dc510a83e42ca3e20c526320e52d34c
Alan Opazo Garay <94488778+alanpogdev@users.noreply.github.com>
2025-08-22 13:28:23 -0400

    Add .next/static/chunks/src_app_crm_opportunities_page_tsx_c7f3e17c._.js 1755883703112

---
+++ .next/static/chunks/src_app_crm_opportunities_page_tsx_c7f3e17c._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_31916d04._.js",
+  "static/chunks/node_modules_80e413b4._.js"
+],
+    source: "dynamic"
+});
